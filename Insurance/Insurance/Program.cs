8f938d8803d40308632a342ebf804709b769d408
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-07-23 09:46:42 -0700

    Insurance Approval

---
+++ Insurance/Insurance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            int yrAge = Convert.ToInt32(yourAge);
+            Console.Read();
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string DUI = Console.ReadLine();
+            Console.ReadLine();
+
+            Console.WriteLine("How many Speeding Tickets do you have?");
+            string yourTickets = Console.ReadLine();
+            int yrTickets = Convert.ToInt32(yourTickets);
+            Console.Read();
+
+            Console.WriteLine("Qualified?");
+            bool isQualified = (yrAge >= 15 && DUI == "no" && yrTickets <=3) ;
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+        }
+    }
+}
