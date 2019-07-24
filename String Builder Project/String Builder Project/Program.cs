a8f0520e2f4c1d890466e4e95d8e8d2a863e01d0
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-07-24 03:36:23 -0700

    String 97

---
+++ String Builder Project/String Builder Project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace String_Builder_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Bob";
+            //string word = " says";
+            //string word2 = " Hellooo....!!";
+            //Console.WriteLine(name + word + word2);
+            //Console.ReadLine();
+
+            //string name = "billy";
+            //name = name.ToUpper();
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Jesse.");
+            sb.Append("\nI like to ride a bicycle.");
+            sb.Append("\nI also enjoy ice cream.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
