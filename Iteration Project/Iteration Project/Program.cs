57da9d673ad553d63184d99fb00cd79122082b25
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-07-24 08:49:37 -0700

    Building Iteration Project

--- Iteration Project/Iteration Project/Program.cs
+++ Iteration Project/Iteration Project/Program.cs
@@ -5,6 +5,51 @@ using System.Collections.Generic;
     {
         static void Main(string[] args)
         {
-           
-        }
+        //string[] names = { "Jesse ", "Erik ", "Daniel ", "Adam " };
+        //Console.WriteLine("Please write a last name.");
+        //string lname = Console.ReadLine();
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //     Console.WriteLine(names[j] + lname);
+        //}
+        //Console.ReadLine();
+
+
+        //string[] names = { "Jesse ", "Erik ", "Daniel ", "Adam " };
+        //Console.WriteLine("Please write a last name.");
+        //string lname = Console.ReadLine();
+
+        //for (int j = 0; j < 50; j++)
+        //{
+        //    Console.WriteLine(names[1] + lname);
+        //}
+        //Console.ReadLine();
+
+
+
+        //string[] names = { "Jesse ", "Erik ", "Daniel ", "Adam " };
+        //Console.WriteLine("Please write a last name.");
+        //string lname = Console.ReadLine();
+
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //    Console.WriteLine(names[j] + lname);
+        //}
+        //Console.ReadLine();
+
+
+        //string[] names = { "Jesse ", "Erik ", "Daniel ", "Adam " };
+        //Console.WriteLine("Please write a last name.");
+        //string lname = Console.ReadLine();
+
+        //for (int j = 0; j <= 2; j++)
+        //{
+        //    Console.WriteLine(names[j] + lname);
+        //}
+        //Console.ReadLine();
+
+
+
+
     }
+}
