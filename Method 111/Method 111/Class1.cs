e25a6de1ac47aad253e2e623c79639afb3e9ce3c
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-07-31 11:47:58 -0700

    IP 111

---
+++ Method 111/Method 111/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_111
+{
+    class Class1
+    {
+        public static int Sum(int num1)
+        {
+
+            int result = num1 + 2;
+
+            return result;
+        }
+
+        public static int Multiply(int num2)
+        {
+
+            int result = num2 * 3;
+
+            return result;
+        }
+
+        public static int Divide(int num3)
+        { 
+            int result = num3 / 5;
+
+            return result;
+        }
+
+       
+    }
+    }
+
---
+++ Method 111/Method 111/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Method_111
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a number to add");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+          
+                              
+
+
+            int c = Sum;
+
+             
+            Console.WriteLine("The Value of the sum is " + c);
+
+
+            
+                Console.WriteLine("Please type a number to divide");
+                int Divide1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please type a second number to divide");
+                int Divide2 = Convert.ToInt32(Console.ReadLine());
+
+
+                int h = Divide1;
+                int k = Divide2;
+
+
+                int i = Divide(h, k);
+
+                 
+                Console.WriteLine("The Value of the Division is " + i);
+
+
+            
+                Console.WriteLine("Please type a number to multiply");
+                int Multiply1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please type a second number to multiply");
+                int Multiply2 = Convert.ToInt32(Console.ReadLine());
+
+
+                int d = Multiply1;
+                int e = Multiply2;
+
+
+                int f = Multiply(d, e);
+
+                
+                Console.WriteLine("The Value of the Multiplication is " + f);
+            
+        }
+
+
+    }
+}
