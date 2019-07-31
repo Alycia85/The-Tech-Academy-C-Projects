c77ae247a89a8ece920e3e7bcbec173e60252c1d
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-07-31 16:45:06 -0700

    pass num's

--- Method 111/Method 111/Program.cs
+++ Method 111/Method 111/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace Method_111
 {
     class Program
@@ -10,29 +11,18 @@ namespace Method_111
             Console.WriteLine("Please type a number to add");
             int num1 = Convert.ToInt32(Console.ReadLine());
 
-          
-                              
-
-
-            int c = Sum;
-
-             
+            Class1 math = new Class1();
+            
+            int c = math.Sum(num1);
+                         
             Console.WriteLine("The Value of the sum is " + c);
 
 
             
                 Console.WriteLine("Please type a number to divide");
-                int Divide1 = Convert.ToInt32(Console.ReadLine());
+                int num3 = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Please type a second number to divide");
-                int Divide2 = Convert.ToInt32(Console.ReadLine());
-
-
-                int h = Divide1;
-                int k = Divide2;
-
-
-                int i = Divide(h, k);
+                int i = math.Divide(num3);
 
                  
                 Console.WriteLine("The Value of the Division is " + i);
@@ -40,17 +30,11 @@ namespace Method_111
 
             
                 Console.WriteLine("Please type a number to multiply");
-                int Multiply1 = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Please type a second number to multiply");
-                int Multiply2 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
 
-                int d = Multiply1;
-                int e = Multiply2;
 
-
-                int f = Multiply(d, e);
+                int f = math.Multiply(num2);
 
                 
                 Console.WriteLine("The Value of the Multiplication is " + f);
