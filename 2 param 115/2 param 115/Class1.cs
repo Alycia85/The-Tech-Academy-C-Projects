3ff51e025de9bd39623966df4eb417c7338b787d
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-08-01 15:04:25 -0700

    complete 115

--- 2 param 115/2 param 115/Class1.cs
+++ 2 param 115/2 param 115/Class1.cs
@@ -6,5 +6,13 @@ namespace _2_param_115
 {
     class Class1
     {
+
+        public int Addition (int asds, int num = 1)
+        {
+           
+            int result = num + (asds + 5);
+            return result;
+        }
+
     }
 }
--- 2 param 115/2 param 115/Program.cs
+++ 2 param 115/2 param 115/Program.cs
@@ -6,7 +6,28 @@ namespace _2_param_115
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Class1 math = new Class1();
+
+
+
+            Console.WriteLine("Please type a number to Add");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please type a number to Add or press enter; this second number is optional.");
+           string num = (Console.ReadLine());
+
+            if (string.IsNullOrEmpty(num))
+            {
+               
+                Console.WriteLine(math.Addition(num1));
+            }
+
+            else
+            {
+                int i = Convert.ToInt32(num);
+                Console.WriteLine("Sum:" + math.Addition(num1, i));
+            }
+            Console.ReadLine();
         }
     }
 }
