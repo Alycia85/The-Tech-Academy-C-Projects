4c183815cadb56a76b553a5ac1a4a960c41b2f22
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-08-01 13:40:05 -0700

    115 start

---
+++ 2 param 115/2 param 115/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2_param_115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
