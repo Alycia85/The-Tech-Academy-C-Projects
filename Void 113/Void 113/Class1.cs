d4256c62767468f276760aa54f5cf59d4752a4bc
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-07-31 17:22:08 -0700

    step 1

---
+++ Void 113/Void 113/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Void_113
+{
+    class Class1
+    {
+        public void Divide (out int math)
+        {
+
+            int h = 100;
+
+            math = 50;
+            int  div = h / math;
+
+           
+            Console.WriteLine("The second number: {0}", math);
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ Void 113/Void 113/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Void_113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
