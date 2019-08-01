a269a7886afb3a45cf17367cbc5a138d3d2d15a0
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-07-31 22:29:25 -0700

    Class Req. Complete

---
+++ Math 114/Math 114/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_114
+{
+    class Class1
+    {
+
+        public int Divide(int num3)
+        {
+            int result = num3 / 5;
+
+            return result;
+        }
+
+
+
+        public int Divide(double num3)
+        {
+            int result = Convert.ToInt32(num3 + 5);
+
+            return result;
+        }
+
+
+        public int Divide(string num3)
+        {
+            int i = Convert.ToInt32(num3);
+            int result = i * 5;
+
+            return result;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Math 114/Math 114/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Math_114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 math = new Class1();
+
+            Console.WriteLine("Please type a number to divide");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int i = math.Divide(num3);
+
+
+            Console.WriteLine("The Value of the Division is " + i);
+        }
+    }
+}
