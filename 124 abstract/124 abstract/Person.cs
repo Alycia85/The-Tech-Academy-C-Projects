7848aa950022199a0b7ee89fdc3a47c4bf9bb008
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-08-13 02:59:24 -0700

    create abstract class

---
+++ 124 abstract/124 abstract/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _124_abstract
+{
+    class Person
+    {
+    }
+}
---
+++ 124 abstract/124 abstract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _124_abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
