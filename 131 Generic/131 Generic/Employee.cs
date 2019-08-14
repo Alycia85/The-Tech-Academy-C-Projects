6e4f3350c04bc977c48632ddd099966545b15308
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-08-13 20:18:27 -0700

    create and complete 131 generic

---
+++ 131 Generic/131 Generic/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _131_Generic
+{
+    public class Employee<T>
+    {
+        List<T> things { get; set; }
+        public int Number { get; set; }
+        public string Word { get; set; }
+
+        public void ListThings()
+        {
+
+            foreach (T Thing in things)
+            {
+                Console.WriteLine(Thing);
+            }
+        }
+    }
+}
+
+
---
+++ 131 Generic/131 Generic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _131_Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> str = new Employee<string>();
+         
+            Employee<int> num = new Employee<int>();
+            
+
+           
+        }
+    }
+}
