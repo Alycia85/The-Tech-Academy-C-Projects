e0e29bcd6ec792735227e51b518d4c36c19fed08
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-08-19 16:55:20 -0700

    debugging
    add for each to program
    take for each from class
    add console. read and write(s)

--- 131 Generic/131 Generic/Employee.cs
+++ 131 Generic/131 Generic/Employee.cs
@@ -6,18 +6,11 @@ namespace _131_Generic
 {
     public class Employee<T>
     {
-        List<T> things { get; set; }
+       public List<T> things { get; set; }
         public int Number { get; set; }
         public string Word { get; set; }
 
-        public void ListThings()
-        {
-
-            foreach (T Thing in things)
-            {
-                Console.WriteLine(Thing);
-            }
-        }
+    
     }
 }
 
--- 131 Generic/131 Generic/Program.cs
+++ 131 Generic/131 Generic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _131_Generic
 {
@@ -7,9 +8,32 @@ namespace _131_Generic
         static void Main(string[] args)
         {
             Employee<string> str = new Employee<string>();
+
+            str.things = new List<string>()
+            {
+                "Bob",
+                "Jo"
+            };
          
             Employee<int> num = new Employee<int>();
-            
+
+            num.things = new List<int>()
+            {
+                5, 7, 10
+            };
+
+          
+
+                foreach (string Thing in str.things)
+                {
+                    Console.WriteLine(Thing);
+                }
+           
+                foreach (int Thing in num.things)
+                {
+                Console.WriteLine(Thing);
+                }
+            Console.ReadLine();
 
            
         }
