eb7a7a34b3ec00882ca3127e53fef1decf39bc11
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-08-19 19:08:54 -0700

    134 enum

--- 134 enum/134 enum/Program.cs
+++ 134 enum/134 enum/Program.cs
@@ -6,7 +6,36 @@ namespace _134_enum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the day of the week.");
+            string day = Console.ReadLine();
+
+            try
+            {
+                DaysOfTheWeek days = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), day);
+                Console.WriteLine("You have entered: " + days);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
         }
+            
+            
+            
+
+            public enum DaysOfTheWeek
+            {
+                Monday,
+                Tuesday,
+                Wednesday,
+                Thursday,
+                Friday,
+                Saturday,
+                Sunday
+
+            }
+    
     }
 }
+
