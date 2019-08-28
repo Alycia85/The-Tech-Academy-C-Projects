037b3d1b20d8704289dc090db57eded605823b3f
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-08-27 17:27:13 -0700

    create and complete 137 Struct

---
+++ 137 Struct/137 Struct/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _137_Struct
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ 137 Struct/137 Struct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _137_Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 5;
+
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
