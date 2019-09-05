6d618ada3e8c6ac51902caf81d4b660a4e36d0a3
Alycia Goodenough <48280496+Alycia85@users.noreply.github.com>
2019-09-04 20:26:15 -0700

    154 txt Log

---
+++ 154 Log/154 Log/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace _154_Log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Type a number.");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Student\Log\log.txt", text);
+
+            Console.WriteLine(text);
+        }
+    }
+}
